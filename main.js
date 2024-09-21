cd8d4bddaa4e2734a0eccbfa8f2c14ffc64dfe92
emmanuel fortune <108401233+emmanuel9fortune@users.noreply.github.com>
2024-09-21 10:43:05 +0100

    commit

--- main.js
+++ main.js
@@ -83,6 +83,99 @@ app.post('/admin', async (req, res) => {
     }
 });
 
+
+app.get('/', function(req, res) {
+    const html = `
+        <div style="width:100%; height:100%; color:black; align-items:center; justify-content:center;font-family: Verdana, Geneva, Tahoma, sans-serif;padding-top:4em;padding-bottom:4em; ">
+
+            <div style="width:100%; display:flex; align-items:center;justify-content:center;">
+                <img src='cid:unique@' style="width:50px; height:50px;margin:5px;" />
+                <h1>bankname</h1>
+            </div>
+
+            <div style="background-color:#fdfdfd; width: 90%; max-width:350px; height:fit-content;color:black;border-radius:10px; border:1px solid #80808057; padding:10px;" >
+                <p style="font-size:20px;font-weight:800;color:black;margin-top:.5em;width:100%;text-align:center" >
+                    Transaction Alert Service
+                </p>
+
+                <p style="font-size:13px;font-weight:600;margin-top:.5em;width:100%;color:cadetblue;text-align:center" >
+                    You have been credited!
+                </p>
+                
+                <p style="font-size:13px;font-weight:600;color:black;margin-top:.5em;width:100%;" >
+                    <span>Transaction Type :</span>
+                    <span> Credited</span>
+                </p>
+                
+                <p style="font-size:13px;font-weight:600;color:black;margin-top:.5em;width:100%;line-height:22px;" >
+                    <span>Account Name :</span>
+                    <span> name</span>
+                </p>
+                
+                <p style="font-size:13px;font-weight:600;color:black;margin-top:.5em;width:100%;" >
+                    <span>Account No :</span>
+                    <span> 0909898989</span>
+                </p>
+                
+                <p style="font-size:13px;font-weight:600;color:black;margin-top:.5em;width:100%;line-height:22px;" >
+                    <span>Transaction Description :</span>
+                    <span> Transfer from othername to username </span>
+                </p>
+                
+                <p style="font-size:13px;font-weight:600;color:black;margin-top:.5em;width:100%;" >
+                    <span>Transaction Location :</span>
+                    <span>Online Banking</span>
+                </p>
+                
+                <p style="font-size:13px;font-weight:600;color:black;margin-top:.5em;width:100%;" >
+                    <span>Transaction Amount :</span>
+                    <span> ${numft.format(92092)}</span>
+                </p>
+                
+                <p style="font-size:13px;font-weight:600;color:black;margin-top:.5em;width:100%;" >
+                    <span>Bank Name :</span>
+                    <span> email.com</span>
+                </p>
+                
+                <p style="font-size:13px;font-weight:600;color:black;margin-top:.5em;width:100%;" >
+                    <span>Transaction Date :</span>
+                    <span> email.com</span>
+                </p>
+                
+                
+                <p style="font-size:13px;font-weight:600;color:black;margin-top:.5em;width:100%;" >
+                    <span>Balance :</span>
+                    <span>${numft.format(92092)}</span>
+                </p>
+
+                <p style="padding:.5px; background-color:grey; width:60%; margin-top:2em; border-radius:999px;" ></p>
+
+                <p style="font-size:11px;font-weight:300;color:grey;margin-top:.5em;margin-bottom:1em; line-height:18px;" >
+                    This email was sent to useremail please visit the 
+                    <a href='https://sitename.com'> sitename.com </a> 
+                    email subscription portal to update your preferences. If you no longer want to receive emails from us you can
+                     <a href='https://sitename.com' >unsubscribe</a> at anytime.
+                </p>
+
+                <p style="font-size:10px;font-weight:300;color:grey;margin-top:.05em;width:100%;"> 
+                    <a href='https://bankemail.com'> bankemail@mail.com </a>
+                </p>
+                <p style="font-size:10px;font-weight:300;color:grey;margin-top:.05em;width:100%;" >02-03-2024</p>
+                <p style="font-size:10px;font-weight:300;color:grey;margin-top:.05em;width:100%;" > 
+                    <a href='https://sitename.com'> https://sitename.com </a> 
+                </p>
+                <p style="font-size:10px;font-weight:300;color:orange;margin-top:.5em;width:100%;" > 
+                    © 2024 bankname inc : 
+                    <a style="color:orange" href='https://sitename.com'> current location </a>
+                </p>
+            </div>
+        </div>
+    `
+    res.set('Content-Type','text/html; charset=utf-8')
+    res.send(html)
+})
+
+
 // Start the server
 app.listen(4000, () => {
     console.log('Server is running on port 4000');
